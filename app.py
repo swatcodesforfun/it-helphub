626f3a521154e217040a2945fe6d71d2d8980a52
swatloomgamer <Adam>
2026-07-21 14:28:02 +0100

    Added website navigation and multiple pages.

--- app.py
+++ app.py
@@ -10,6 +10,28 @@ def home():
     return render_template("index.html")
 
 
+
+@app.route("/tools")
+def tools():
+
+    return render_template("tools.html")
+
+
+
+@app.route("/troubleshooting")
+def troubleshooting():
+
+    return render_template("troubleshooting.html")
+
+
+
+@app.route("/learn")
+def learn():
+
+    return render_template("learn.html")
+
+
+
 if __name__ == "__main__":
 
     app.run(debug=True)
